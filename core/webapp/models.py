cb7196d15672f40ca3da3a80cde9dc7af7d30782
Sulatn Roziyev <jjo152065@gmail.com>
2023-02-16 12:57:31 +0600

    Добавил новые поля

--- core/webapp/models.py
+++ core/webapp/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 
 class List(models.Model):
-    description = models.TextField(max_length=3000, null=False, blank=False, verbose_name="Описание")
+    title = models.CharField(max_length=50, null=False, blank=False, verbose_name="Заголовок", default="No Title")
+    description = models.CharField(max_length=100, null=False, blank=False, verbose_name="Описание", default="No description")
+    detailed_description = models.TextField(max_length=2500, null=False, blank=False, verbose_name="Подробное описание", default="Not detail description")
     status = models.CharField(max_length=100, null=False, blank=False, default="New", verbose_name="Статус")
-    date = models.CharField(max_length=50, default="", verbose_name="Дата")
+    date = models.DateField(default="", verbose_name="Дата")
 
     def __str__(self):
         return f"{self.description} {self.status}"
