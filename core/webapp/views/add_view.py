ef732d730c79dd05cbb1ed39788137e3ab28aef1
Sulatn Roziyev <jjo152065@gmail.com>
2023-02-16 14:46:50 +0600

    Изменил код, использовал reverse и redirect

--- core/webapp/views/add_view.py
+++ core/webapp/views/add_view.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, reverse
 from webapp.models import List
 from django.core.handlers.wsgi import WSGIRequest
 
@@ -7,9 +7,12 @@ def add_view(request: WSGIRequest):
     if request.method == 'GET':
         return render(request, 'add_view.html')
     list_data = {
+        'title': request.POST.get('title'),
         'description': request.POST.get('description'),
+        'detailed_description': request.POST.get('detailed_description'),
         'status': request.POST.get('status'),
         'date': request.POST.get('date')
     }
     lists = List.objects.create(**list_data)
-    return redirect(f'/detail_view/?pk={lists.pk}')
+    reverse_url = reverse('detail_view', kwargs={'pk': lists.pk})
+    return redirect(reverse_url)
