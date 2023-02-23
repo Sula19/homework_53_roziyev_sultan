e9d2db14fe725e133274e3ba5cc481cc33a3954e
Sulatn Roziyev <jjo152065@gmail.com>
2023-02-23 14:57:04 +0600

    Изменил модель

--- core/webapp/models.py
+++ core/webapp/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class List(models.Model):
-    title = models.CharField(max_length=50, null=False, blank=False, verbose_name="Заголовок", default="No Title")
-    description = models.CharField(max_length=100, null=False, blank=False, verbose_name="Описание", default="No description")
-    detailed_description = models.TextField(max_length=2500, null=False, blank=False, verbose_name="Подробное описание", default="Not detail description")
-    status = models.CharField(max_length=100, null=False, blank=False, default="New", verbose_name="Статус")
+    title = models.CharField(max_length=50, null=False, blank=False, verbose_name="Заголовок")
+    description = models.CharField(max_length=100, null=False, blank=False, verbose_name="Описание")
+    detailed_description = models.TextField(max_length=2500, null=False, blank=False, verbose_name="Подробное описание")
+    status = models.CharField(max_length=100, null=False, blank=False, verbose_name="Статус")
     date = models.DateField(default="", verbose_name="Дата")
 
     def __str__(self):
